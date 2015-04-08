837f2525eace8c55ba800fdf003ce676680fdebc
Ludovic Thomas <thomasludovic@gmail.com>
2015-04-08 13:22:28 -0400

    feat(NodeJS-Server): Update the supporting files to generate a valid swagger documentation (JS, no more JSON)

--- modules/swagger-codegen/src/main/java/com/mixgenius/swagger/codegen/languages/NodeJSServerMockCodegen.java
+++ modules/swagger-codegen/src/main/java/com/mixgenius/swagger/codegen/languages/NodeJSServerMockCodegen.java
@@ -12,6 +12,24 @@ public class NodeJSServerMockCodegen extends NodeJSCustomCodegen {
     super();
 
     // Nothing more for now
+    supportingFiles.clear();
+    supportingFiles.add(new SupportingFile("swagger.mustache",
+      "spec",
+      "swagger.js")
+    );
+    supportingFiles.add(new SupportingFile("index.mustache",
+      "",
+      "index.js")
+    );
+    supportingFiles.add(new SupportingFile("package.mustache",
+      "",
+      "package.json")
+    );
+    if(System.getProperty("noservice") == null) {
+      apiTemplateFiles.put(
+        "service.mustache",   // the template to use
+        "Service.js");       // the extension for each file to write
+    }
   }
 
   public CodegenType getTag() {
