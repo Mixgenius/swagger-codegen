3e082baca98985d63db8bf1925beac9abcbbdef3
Ludovic Thomas <thomasludovic@gmail.com>
2015-04-29 16:02:35 -0400

    fix(NodeJS-Client): Update the operation nickname to fit Michael needs

--- modules/swagger-codegen/src/main/java/com/mixgenius/swagger/codegen/languages/NodeJSClientCodegen.java
+++ modules/swagger-codegen/src/main/java/com/mixgenius/swagger/codegen/languages/NodeJSClientCodegen.java
@@ -15,6 +15,8 @@ public class NodeJSClientCodegen extends NodeJSCustomCodegen {
     // Clean model templates, and add ours
     modelTemplateFiles.clear();
     modelTemplateFiles.put("model.mustache", ".js");
+    modelTemplateFiles.put("dtos.mustache", "DTO.js");
+    modelTemplateFiles.put("extensions.mustache", "Extension.js");
 
     // Clean api template, and add ours
     apiTemplateFiles.clear();
@@ -37,5 +39,18 @@ public class NodeJSClientCodegen extends NodeJSCustomCodegen {
 
   public CodegenType getTag() {
     return CodegenType.CLIENT;
+  }
+
+  @Override
+  public Map<String, Object> postProcessOperations(Map<String, Object> objs) {
+    Map<String, Object> objectMap = (Map<String, Object>)objs.get("operations");
+    List<CodegenOperation> operations = (List<CodegenOperation>)objectMap.get("operation");
+    for(CodegenOperation operation : operations) {
+      // Ugly code to get the operation nickname, the way Michael wants :P
+      String newNickname = operation.nickname.replace(operation.baseName+"_", "");
+      newNickname = newNickname.substring(0,1).toLowerCase() + newNickname.substring(1);
+      operation.nickname = newNickname;
+    }
+    return objs;
   }
 }
